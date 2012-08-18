bf19d652d0f3f9d018d2d5039a35337bd78a0340
oveddan <oveddan@gmail.com>
2012-08-18 00:06:46 -0400

    started flusing out pulling and parsing stats.

--- extract2012SummaryStats.js
+++ extract2012SummaryStats.js
@@ -1,4 +1,24 @@
+var request = require('request'),
+  $ = require('jQuery'),
+  async = require('async'),
+  _ = require('underscore');
+
 
 module.exports = function extract2012SummaryStats(){
-  
+
+};
+
+var pullStats = function(page, callback){
+  request("http://fantasy.premierleague.com/stats/elements/page=1" + 1, function(err, data){
+    if(err) callback(err);
+    else {
+      var parsedStats = parseStatsFromHtml(data);
+      callback(parsedStats);
+    }
+  });
+};
+
+var parseStatsFromHtml = function(pageHtml){
+  var $playerRows = $(pageHtml).find('#ismTable').find('tr');
+
 }
